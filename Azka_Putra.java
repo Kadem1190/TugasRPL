a0587d5241b559e62d66accae91ddfadb9493833
Kadem <97424456+Kadem1190@users.noreply.github.com>
2023-08-06 12:26:28 +0700

    Update Azka_Putra.java

--- Azka_Putra.java
+++ Azka_Putra.java
@@ -4,7 +4,7 @@ public class Azka_Putra {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Azka Calculator");
+        System.out.println("Simple Calculator \n Made By Azka X - RPL");
         System.out.println("Enter the first number:");
         double num1 = scanner.nextDouble();
 
